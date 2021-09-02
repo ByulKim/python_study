e91356d06861cfc65619c6a85c47f497ff2ad785
ByulKim <bbyyuull33@gmail.com>
2021-09-02 17:47:47 +0900

    수정_1

--- test/test_3.py
+++ test/test_3.py
@@ -26,13 +26,40 @@ def solution(new_id):
     new_id = re.sub(r"[^a-z0-9-_.]","",new_id)
 
     #3단계
+    while ".." in new_id:
+        new_id = new_id.replace("..",".")
 
+    #4단계
+    if len(new_id) > 0:
+        while (new_id[0] == "."): #시작부분
+            new_id = new_id[1:]
+
+        while (new_id[len(new_id) -1] == "."): #끝부분
+            new_id = new_id[0:len(new_id) -1]
+
+    #5단계
+    if new_id == "":
+        new_id = "a"
+
+    #6단계
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+    
+    while (new_id[0] == "."): #시작부분
+        new_id = new_id[1:]
+
+    while (new_id[len(new_id) -1] == "."): #끝부분
+        new_id = new_id[0:len(new_id) -1]
+
+    #7단계
+    while len(new_id) < 3:
+        new_id = new_id + new_id[-1]
 
     print(new_id)
 
     return answer
 
-solution("...!@BaT#*..y.abcdefghijklm")
+solution("=.=")
 
 #no	new_id	result
 #예1	"...!@BaT#*..y.abcdefghijklm"	"bat.y.abcdefghi"
